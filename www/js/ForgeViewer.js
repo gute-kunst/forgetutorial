b099064cfffff6a5b2c7a286f696d62834073ff7
gutekunst <johannes.gutekunst@t-online.de>
2018-07-20 19:37:40 +0200

    custom extensions

--- www/js/ForgeViewer.js
+++ www/js/ForgeViewer.js
@@ -8,7 +8,8 @@ function launchViewer(urn) {
   var documentId = 'urn:' + urn;
   Autodesk.Viewing.Initializer(options, function onInitialized() {
     viewerApp = new Autodesk.Viewing.ViewingApplication('forgeViewer');
-    viewerApp.registerViewer(viewerApp.k3D, Autodesk.Viewing.Private.GuiViewer3D);
+    // THE FOLLOWING LINE WAS REPLACED DUE TO EXTENSION
+    viewerApp.registerViewer(viewerApp.k3D, Autodesk.Viewing.Private.GuiViewer3D, { extensions: ['HandleSelectionExtension'] });
     viewerApp.loadDocument(documentId, onDocumentLoadSuccess, onDocumentLoadFailure);
   });
 }
